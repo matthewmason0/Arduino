f0a6c9ba95e1dec6a0acc53fa30ffbba6682167d
matthewmason0 <matthewmason0@gmail.com>
2016-04-03 10:07:34 -0400

    Constructor and bitRead methods

--- libraries/Mbus/Nibble.cpp
+++ libraries/Mbus/Nibble.cpp
@@ -3,3 +3,18 @@
   Created by Matthew Mason, April 3, 2016.
   !(Released into the public domain.)
 */
+
+boolean bits[4];
+
+Nibble:Nibble()
+{
+}
+
+boolean bitRead(int bit)
+{
+     return bits[bit];
+}
+
+void bitWrite(int bit)
+{
+     bits[bit]=
