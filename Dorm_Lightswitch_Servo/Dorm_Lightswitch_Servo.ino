816bca74d1916cfaeb7348547a9fd8a4dec826a4
Matt Mason <matthewmason0@gmail.com>
2017-11-02 16:56:29 -0500

    Add door unlock sensor to dorm lights

--- Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
+++ Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
@@ -1,8 +1,11 @@
 #include <Servo.h>
 Servo servo;
 
+bool locked = false;
+
 void setup() {
   pinMode(2, INPUT);
+  pinMode(4, INPUT);
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   servo.attach(3);
@@ -11,10 +14,23 @@ void setup() {
 
 void loop() {
   if (digitalRead(2)) {
-    servo.write(180);
-    delay(1500);
-    servo.write(0);
-    delay(1000*60);
+    lightsOn();
+    delay(60000);
   }
+  
+  if (digitalRead(4))
+    locked = true;
+  else if(locked) {
+      lightsOn();
+      locked = false;
+  }
+  
   delay(100);
 }
+
+void lightsOn() {
+  servo.write(170);
+  delay(1500);
+  servo.write(0);
+}
+
