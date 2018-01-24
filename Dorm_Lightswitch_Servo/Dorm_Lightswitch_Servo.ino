924545bf1f1a5a5a96459c82a46a31c0298c9733
matthewmason0 <matthewmason0@gmail.com>
2018-01-24 10:41:11 -0600

    Update Dorm_Lightswitch_Servo.ino

--- Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
+++ Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
@@ -1,45 +1,27 @@
 #include <Servo.h>
-Servo servo;
 
-bool locked = false;
+Servo mlon;
+Servo mloff;
+Servo unlock;
+Servo lock;
 
 void setup() {
-  pinMode(2, INPUT);
-  pinMode(4, INPUT);
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
-  servo.attach(3);
-  servo.write(0);
+
+  Serial.begin(9600);
+
+  mlon.attach(3);
+  mloff.attach(4);
+  unlock.attach(5);
+  lock.attach(6);
+
+  mlon.write(0);
 }
 
 void loop() {
-  if (digitalRead(2)) {
-    lightsOn();
-    delay(60000);
+    
   }
-  
-  if (digitalRead(4))
-    locked = true;
-  else if(locked) {
-      digitalWrite(13, HIGH);
-      delay(500);
-      if(digitalRead(4)) //if circuit reconnected within .5 s, ignore
-        return;
-      digitalWrite(13, LOW);
-      delay(200);
-      digitalWrite(13, HIGH);
-      lightsOn();
-      locked = false;
-  }
-
-  digitalWrite(13, LOW);
   
   delay(100);
 }
-
-void lightsOn() {
-  servo.write(170);
-  delay(1500);
-  servo.write(0);
-}
-
