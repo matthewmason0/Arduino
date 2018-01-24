3367cf78879292467cb47375ce2ae0c18bbdf862
Matt Mason <matthewmason0@gmail.com>
2018-01-23 23:59:12 -0600

    Update dorm server with new features

--- Dorm_Lights_Server/Dorm_Lights_Server.ino
+++ Dorm_Lights_Server/Dorm_Lights_Server.ino
@@ -1,134 +1,181 @@
-//zoomkat 9-5-11
-//routerbot code
-//for use with IDE 0021
-//open serial monitor to see what the arduino receives
-//use the \ slash to escape the " in the html 
-//address will look like http://192.168.1.102:84/ when submited
-//for use with W5100 based ethernet shields
-
-#include <SPI.h>
 #include <Ethernet.h>
+#include <SoftwareSerial.h>
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x2C, 0x1E }; //physical mac address
-//IPAddress ip(10,0,0,20); // ip in lan
-//byte gateway[] = { 10, 0, 0, 1 }; // internet access via router
-//byte subnet[] = { 255, 255, 255, 0 }; //subnet mask
 EthernetServer server(80); //server port
 
+SoftwareSerial swSerial(5, 6); //RX(yellow), TX(green)
+
+//Serial = rpi
+//swSerial = servo arduino
+
 String readString; 
-int pinState;
 
 //////////////////////
 
-void setup(){
-  pinMode(8, OUTPUT); //pin selected to control
+void setup()
+{
+  pinMode(8, OUTPUT); //Christmas Lights
+  digitalWrite(8, LOW);
+  pinMode(3, INPUT); //Alarm
 
-  //enable serial data print 
-  Serial.begin(9600); 
-  Serial.println("dorm lights initializing..."); // so I can keep track of what is loaded
+  Serial.begin(9600);
+  swSerial.begin(9600);
+  
+//  Serial.println("dorm control initializing...");
   
   //start Ethernet
-  if(Ethernet.begin(mac)==0){ //ip);
-    Serial.println("DHCP Failed");
+  if(Ethernet.begin(mac)==0)
+  {
+//    Serial.println("DHCP Failed");
     while(true);
   }
-  Serial.print("Acquired IP Address: ");
-  Serial.println(Ethernet.localIP());
+//  Serial.print("Acquired IP Address: ");
+//  Serial.println(Ethernet.localIP());
   server.begin();
 }
 
-void loop(){
+void loop()
+{
+  if(digitalRead(2)) //Alarm
+  {
+    swSerial.println("mlon");
+    delay(60000);
+  }
+  
   byte result = Ethernet.maintain();
-  if(result==1||result==3){
-    Serial.println("DHCP Failed");
+  if(result==1||result==3)
+  {
+//    Serial.println("DHCP Failed");
     while(true);
   }
-  else if(result==2){ //IP renewed
-    Serial.print("Lease renewed for IP Address: ");
-    Serial.print(Ethernet.localIP());
+  else if(result==2) //IP renewed
+  {
+//    Serial.print("Lease renewed for IP Address: ");
+//    Serial.print(Ethernet.localIP());
   }
-  else if(result==4){ //new IP bound
-    Serial.print("Acquired new IP Address: ");
-    Serial.println(Ethernet.localIP());
+  else if(result==4) //new IP bound
+  {
+//    Serial.print("Acquired new IP Address: ");
+//    Serial.println(Ethernet.localIP());
   }
   
   // Create a client connection
   EthernetClient client = server.available();
-  if (client) {    
-    while (client.connected()) {
-      if (client.available()) {
+  if (client)
+  {    
+    while (client.connected())
+    {
+      if (client.available())
+      {
         char c = client.read();
 
-        //read char by char HTTP request
-        if (readString.length() < 100) {
-
+        //read char-by-char HTTP request
+        if (readString.length() < 100)
+        {
           //store characters to string 
           readString += c; 
-          Serial.print(c);
         } 
 
         //if HTTP request has ended
-        if (c == '\n') {
-          Serial.println(readString); //print to serial monitor for debuging 
-
-          //now output HTML data header
-          if(readString.indexOf('?') >=0) { //don't send new page
-            client.println("HTTP/1.1 204 Zoomkat");
+        if (c == '\n')
+        {
+          int index = readString.indexOf('?');
+          if(index >=0)
+          { 
+            client.println("HTTP/1.1 204 No Content"); //don't send new page
             client.println();
-            client.println();  
+            client.println();
+            
+            String command = readString.substring(index+1, readString.length()-11);
+                        
+            if(command.equals("unlock"))
+              swSerial.println("unlock");
+              
+            if(command.equals("lock"))
+              swSerial.println("lock");
+              
+            if(command.equals("mlon"))
+              swSerial.println("mlon");
+              
+            if(command.equals("mloff"))
+              swSerial.println("mloff");
+              
+            if(command.equals("clon"))
+              digitalWrite(8, HIGH);
+              
+            if(command.equals("cloff"))
+              digitalWrite(8, LOW);
+              
+            if(command.equals("tvon"))
+              Serial.println("tvon");
+              
+            if(command.equals("tvoff"))
+              Serial.println("tvoff");
+              
+            if(command.equals("hdmi1"))
+              Serial.println("hdmi1");
+              
+            if(command.equals("hdmi2"))
+              Serial.println("hdmi2");
+              
           }
-          else {
-            client.println("HTTP/1.1 200 OK"); //send new page
-            client.println("Content-Type: text/html");
-          //client.println("Refresh: 10");
+          else
+          {
+            client.println(F("HTTP/1.1 200 OK")); //send new page
+            client.println(F("Content-Type: text/html"));
+//            client.println(F("Refresh: 10"));
             client.println();
   
-            client.println("<HTML>");
-            client.println("<HEAD>");
-            client.println("<link rel=\"icon\" type=\"image/png\" href=\"https://cdn4.iconfinder.com/data/icons/mosaicon-08/512/cloud_settings-128.png\">");
-            client.println("<TITLE>Lights Control</TITLE>");
-            client.println("</HEAD>");
-            client.println("<BODY>");
-  
-            client.println("<H1><font face=\"verdana\">Lights Control</font></H1>");
+            client.println(F("<HTML>"));
             
-            client.println("<a href=\"/?on\" target=\"inlineframe\"><font size=\"5\" color=\"#00E600\">Turn On</font></a>"); 
-            client.print("&nbsp;&nbsp;&nbsp;");
-            client.print("<a href=\"/?off\" target=\"inlineframe\"><font size=\"5\" color=\"red\">Turn Off</font></a>");
+            client.println(F("<HEAD>"));
+            client.println(F("<link rel=\"icon\" type=\"image/png\" href=\"https://lh6.ggpht.com/07rwQs8Zp0K0R_qBAYJRrbAmMHghgWJnx5MaZR4vl2V5dzPtV7e4nKLLFZdGsK2zg6lF=w300\">"));
+            client.println(F("<TITLE>Control Panel</TITLE>"));
+            client.println(F("</HEAD>"));
             
-//          if(pinState) client.println("<H4>Current State: <font face=\"verdana\" color=\"#00E600\">ON</font></H4>");
-//          else if(pinState==0) client.println("<H4>Current State: <font face=\"verdana\" color=\"yellow\">OFF</font></H4>");
-//          else client.println("<H4>Current State: <font face=\"verdana\" color=\"red\">ERROR</font></H4>");
+            client.println(F("<BODY>"));
+            client.println(F("<font face=\"verdana\">"));
   
-          //client.println("<IFRAME name=inlineframe src=\"res://D:/WINDOWS/dnserror.htm\" width=1 height=1\">");
-            client.println("<IFRAME name=inlineframe style=\"display:none\" >");          
-            client.println("</IFRAME>");
-  
-            client.println("</BODY>");
-            client.println("</HTML>");
+            client.println(F("<h1>Control Panel</h1>"));
+            client.println(F("<p>&nbsp;</p>"));
+
+            client.println(F("<h3>Door:</h3>"));
+            client.println(F("<h3><a href=\"/?unlock\"><font color=\"#00E600\">Unlock</font></a>")); 
+            client.println(F("&nbsp;&nbsp;&nbsp;"));
+            client.println(F("<a href=\"/?lock\"><font color=\"red\">Lock</font></a></h3>"));
+
+            client.println(F("<h3>Main Light:</h3>"));
+            client.println(F("<h3><a href=\"/?mlon\"><font color=\"#00E600\">Turn On</font></a>")); 
+            client.println(F("&nbsp;&nbsp;&nbsp;"));
+            client.println(F("<a href=\"/?mloff\"><font color=\"red\">Turn Off</font></a></h3>"));
+
+            client.println(F("<h3>Christmas Lights:</h3>"));
+            client.println(F("<h3><a href=\"/?clon\"><font color=\"#00E600\">Turn On</font></a>")); 
+            client.println(F("&nbsp;&nbsp;&nbsp;"));
+            client.println(F("<a href=\"/?cloff\"><font color=\"red\">Turn Off</font></a></h3>"));
+
+            client.println(F("<h3>TV:</h3>"));
+            client.println(F("<h3><a href=\"/?tvon\"><font color=\"#00E600\">Turn On</font></a>")); 
+            client.println(F("&nbsp;&nbsp;&nbsp;"));
+            client.println(F("<a href=\"/?tvoff\"><font color=\"red\">Turn Off</font></a></h3>"));
+            client.println(F("<h3><a href=\"/?hdmi1\">HDMI 1</a>")); 
+            client.println(F("&nbsp;&nbsp;&nbsp;"));
+            client.println(F("<a href=\"/?hdmi2\">HDMI 2</a></h3>"));
+
+            client.println(F("</font>"));
+            client.println(F("</BODY>"));
+            client.println(F("</HTML>"));
           }
 
           delay(1);
           //stopping client
           client.stop();
 
-          ///////////////////// control arduino pin
-          if(readString.indexOf("?on") >0)//checks for on
-          {
-            digitalWrite(8, HIGH);
-            Serial.println("Led On");
-            pinState = true;
-          }
-          else if(readString.indexOf("?off") >0)//checks for off
-          {
-            digitalWrite(8, LOW);
-            Serial.println("Led Off");
-            pinState = false;
-          }
           //clearing string for next read
           readString="";
         }
       }
     }
   }
-} 
+}
