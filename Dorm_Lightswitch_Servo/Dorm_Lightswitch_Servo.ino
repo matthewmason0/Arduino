3607ee9f9277d797c5e6523b9e93a13b6f60677e
Matt Mason <matthewmason0@gmail.com>
2018-02-10 01:58:13 -0600

    change servos to n.detached

--- Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
+++ Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
@@ -8,12 +8,17 @@ Servo lock;
 
 DHT dht(12, DHT11);
 
+static constexpr int MLON = 3;
+static constexpr int MLOFF = 4;
+static constexpr int UNLOCK = 5;
+static constexpr int LOCK = 6;
+
 int mlon_range[]   = {0, 170}; //normal, activated
 int mloff_range[]  = {0, 165};
 int unlock_range[] = {160, 0};
 int lock_range[]   = {0, 170};
 
-int activation_time = 1500;
+int slew_time = 400;
 
 unsigned long currentTime;
 unsigned long previousTransmit = 0;
@@ -32,20 +37,25 @@ void setup()
   digitalWrite(13, LOW);
 
   Serial.begin(9600);
-
-  mlon.attach(3);
-  mloff.attach(4);
-  unlock.attach(5);
-  lock.attach(6);
-
+  
   dht.begin();
+  
+  mlon.attach(MLON);
+  mloff.attach(MLOFF);
+  unlock.attach(UNLOCK);
+  lock.attach(LOCK);
 
   mlon.write(mlon_range[0]);
   mloff.write(mloff_range[0]);
   unlock.write(unlock_range[0]);
   lock.write(lock_range[0]);
+  
+  delay(slew_time);
 
+  mlon.detach();
+  mloff.detach();
   unlock.detach();
+  lock.detach();
 }
 
 void loop()
@@ -68,53 +78,71 @@ void loop()
     command.trim();
     if (command.equals("unlockmlon"))
     {
-      unlock.attach(5);
+      unlock.attach(UNLOCK);
+      mlon.attach(MLON);
       unlock.write(unlock_range[1]);
       mlon.write(mlon_range[1]);
-      delay(activation_time);
+      delay(slew_time);
       unlock.write(unlock_range[0]);
       mlon.write(mlon_range[0]);
+      delay(slew_time);
       unlock.detach();
+      mlon.detach();
     }
     if (command.equals("lockmloff"))
     {
+      lock.attach(LOCK);
+      mloff.attach(MLOFF);
       lock.write(lock_range[1]);
       mloff.write(mloff_range[1]);
-      delay(activation_time);
+      delay(slew_time);
       lock.write(lock_range[0]);
       mloff.write(mloff_range[0]);
+      delay(slew_time);
+      lock.detach();
+      mloff.detach();
     }
     if (command.equals("mlon"))
     {
+      mlon.attach(MLON);
       mlon.write(mlon_range[1]);
-      delay(activation_time);
+      delay(slew_time);
       mlon.write(mlon_range[0]);
+      delay(slew_time);
+      mlon.detach();
     }
     if (command.equals("mloff"))
     {
+      mloff.attach(MLOFF);
       mloff.write(mloff_range[1]);
-      delay(activation_time);
+      delay(slew_time);
       mloff.write(mloff_range[0]);
+      delay(slew_time);
+      mloff.detach();
     }
     if (command.equals("unlock"))
     {
-      unlock.attach(5);
+      unlock.attach(UNLOCK);
       unlock.write(unlock_range[1]);
-      delay(activation_time);
+      delay(slew_time);
       unlock.write(unlock_range[0]);
+      delay(slew_time);
       unlock.detach();
     }
     if (command.equals("lock"))
     {
+      lock.attach(LOCK);
       lock.write(lock_range[1]);
-      delay(activation_time);
+      delay(slew_time);
       lock.write(lock_range[0]);
+      delay(slew_time);
+      lock.detach();
     }
     command = "";
     commandAvailable = false;
   }
   
-  delay(10);
+  delay(1);
 }
 
 void serialEvent()
