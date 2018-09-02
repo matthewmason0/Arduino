4c769433f29ed0de2a7bffbdde4844eb4fa2dc1a
Matthew Mason <matthewmason0@gmail.com>
2018-09-02 00:29:04 -0500

    Alpha homebridge

--- Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
+++ Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
@@ -1,29 +1,36 @@
 #include <Servo.h>
-#include <DHT.h>
+//#include <DHT.h>
+
+static constexpr int NUM_SERVOS = 6;
 
 Servo mlon;
 Servo mloff;
-Servo unlock;
-Servo lock;
+Servo fon;
+Servo foff;
+Servo clon;
+Servo cloff;
 
-DHT supplyDHT(12, DHT11);
-DHT spaceDHT(10, DHT11);
+enum Servos      { MLON, MLOFF, FON, FOFF, CLON, CLOFF };
+Servo servos[] = { mlon, mloff, fon, foff, clon, cloff };
 
-static constexpr int MLON = 3;
-static constexpr int MLOFF = 4;
-static constexpr int UNLOCK = 5;
-static constexpr int LOCK = 6;
+enum Info { PIN, NORMAL, ACTIVATED, SLEW };
+int info[][4] = { { 2, 70,  130, 500 },
+                  { 3, 90,  10,  600 },
+                  { 4, 85,  120, 500 },
+                  { 5, 90,  165, 300 },
+                  { 6, 130, 170, 200 },
+                  { 7, 90,  165, 300 } };
 
-int mlon_info[]   = {130, 170, 200}; //normal, activated, individual slew time
-int mloff_info[]  = {90, 165,  300};
-int unlock_info[] = {160, 0,   500};
-int lock_info[]   = {0, 170,   400};
+static constexpr int HL = 8;
 
-int slew_time = 500;
+static constexpr int SLEW_TIME = 500;
 
-unsigned long currentTime;
-unsigned long previousTransmit = 0;
-unsigned long transmitInterval = 2000;
+//DHT supplyDHT(12, DHT11);
+//DHT spaceDHT(10, DHT11);
+
+//unsigned long currentTime;
+//unsigned long previousTransmit = 0;
+//unsigned long transmitInterval = 2000;
 
 String command = "";
 bool commandAvailable = false;
@@ -32,38 +39,45 @@ void setup()
 {
   command.reserve(200);
 
-  pinMode( 9, OUTPUT);
-  pinMode(11, OUTPUT);
+  //pinMode( 9, OUTPUT);
+  //pinMode(11, OUTPUT);
+  pinMode(HL, OUTPUT);
   pinMode(13, OUTPUT);
-  digitalWrite( 9, HIGH);
-  digitalWrite(11, HIGH);
+  //digitalWrite( 9, HIGH);
+  //digitalWrite(11, HIGH);
+  digitalWrite(HL, LOW);
   digitalWrite(13, LOW);
 
   Serial.begin(9600);
   
-  supplyDHT.begin();
-  spaceDHT.begin();
+  //supplyDHT.begin();
+  //spaceDHT.begin();
+ 
+  for (int i = 0; i < NUM_SERVOS; i++)
+  {
+    servos[i].attach(info[i][PIN]);
+    servos[i].write(info[i][NORMAL]);
+  }
   
-  mlon.attach(MLON);
-  mloff.attach(MLOFF);
-  unlock.attach(UNLOCK);
-  lock.attach(LOCK);
+  delay(SLEW_TIME);
 
-  mlon.write(mlon_info[0]);
-  mloff.write(mloff_info[0]);
-  unlock.write(unlock_info[0]);
-  lock.write(lock_info[0]);
-  
-  delay(slew_time);
+  for (int i = 0; i < NUM_SERVOS; i++)
+    servos[i].detach();
+}
 
-  mlon.detach();
-  mloff.detach();
-  unlock.detach();
-  lock.detach();
+void activateServo(Servos s)
+{
+  servos[s].attach(info[s][PIN]);
+  servos[s].write(info[s][ACTIVATED]);
+  delay(info[s][SLEW]);
+  servos[s].write(info[s][NORMAL]);
+  delay(info[s][SLEW]);
+  servos[s].detach();
 }
 
 void loop()
 { 
+/*
   currentTime = millis();
   if(currentTime - previousTransmit > transmitInterval)
   {
@@ -79,72 +93,27 @@ void loop()
       previousTransmit = currentTime;
     }
   }
+*/
   
   if (commandAvailable)
   {
     command.trim();
-    if (command.equals("unlockmlon"))
-    {
-      unlock.attach(UNLOCK);
-      mlon.attach(MLON);
-      unlock.write(unlock_info[1]);
-      mlon.write(mlon_info[1]);
-      delay(slew_time);
-      unlock.write(unlock_info[0]);
-      mlon.write(mlon_info[0]);
-      delay(slew_time);
-      unlock.detach();
-      mlon.detach();
-    }
-    if (command.equals("lockmloff"))
-    {
-      lock.attach(LOCK);
-      mloff.attach(MLOFF);
-      lock.write(lock_info[1]);
-      mloff.write(mloff_info[1]);
-      delay(slew_time);
-      lock.write(lock_info[0]);
-      mloff.write(mloff_info[0]);
-      delay(slew_time);
-      lock.detach();
-      mloff.detach();
-    }
     if (command.equals("mlon"))
-    {
-      mlon.attach(MLON);
-      mlon.write(mlon_info[1]);
-      delay(mlon_info[2]);
-      mlon.write(mlon_info[0]);
-      delay(mlon_info[2]);
-      mlon.detach();
-    }
+      activateServo(MLON);
     if (command.equals("mloff"))
-    {
-      mloff.attach(MLOFF);
-      mloff.write(mloff_info[1]);
-      delay(mloff_info[2]);
-      mloff.write(mloff_info[0]);
-      delay(mloff_info[2]);
-      mloff.detach();
-    }
-    if (command.equals("unlock"))
-    {
-      unlock.attach(UNLOCK);
-      unlock.write(unlock_info[1]);
-      delay(unlock_info[2]);
-      unlock.write(unlock_info[0]);
-      delay(unlock_info[2]);
-      unlock.detach();
-    }
-    if (command.equals("lock"))
-    {
-      lock.attach(LOCK);
-      lock.write(lock_info[1]);
-      delay(lock_info[2]);
-      lock.write(lock_info[0]);
-      delay(lock_info[2]);
-      lock.detach();
-    }
+      activateServo(MLOFF);
+    if (command.equals("fon"))
+      activateServo(FON);
+    if (command.equals("foff"))
+      activateServo(FOFF);
+    if (command.equals("clon"))
+      activateServo(CLON);
+    if (command.equals("cloff"))
+      activateServo(CLOFF);
+    if (command.equals("hlon"))
+      digitalWrite(HL, HIGH);
+    if (command.equals("hloff"))
+      digitalWrite(HL, LOW);
     command = "";
     commandAvailable = false;
   }
