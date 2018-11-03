ebced5a2d7e3819979b983d800021e7cb905779e
Matthew Mason <matthewmason0@gmail.com>
2018-11-03 17:33:56 -0500

    Add servoResetInterval

--- Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
+++ Dorm_Lightswitch_Servo/Dorm_Lightswitch_Servo.ino
@@ -15,8 +15,8 @@ Servo servos[] = { mlon, mloff, fon, foff, clon, cloff };
 
 enum Info { PIN, NORMAL, ACTIVATED, SLEW };
 int info[][4] = { { 2, 70,  150, 600 },   //MLON
-                  { 3, 90,  10,  600 },   //MLOFF
-                  { 4, 85,  120, 500 },   //FON
+                  { 3, 80,  30,  400 },   //MLOFF
+                  { 4, 85,  120, 300 },   //FON
                   { 5, 75,  0,   700 },   //FOFF
                   { 6, 65,  115, 500 },   //CLON
                   { 7, 80,  180, 600 } }; //CLOFF
@@ -28,9 +28,11 @@ static constexpr int SLEW_TIME = 500;
 //DHT supplyDHT(12, DHT11);
 //DHT spaceDHT(10, DHT11);
 
-//unsigned long currentTime;
-//unsigned long previousTransmit = 0;
-//unsigned long transmitInterval = 2000;
+unsigned long servoResetInterval = 500;
+//unsigned long sensorReadInterval = 2000;
+unsigned long currentTime;
+unsigned long previousReset = 0;
+//unsigned long previousRead = 0;
 
 String command = "";
 
@@ -57,27 +59,38 @@ void setup()
     servos[i].attach(info[i][PIN]);
     servos[i].write(info[i][NORMAL]);
   }
-
   delay(SLEW_TIME);
 
-  // for (int i = 0; i < NUM_SERVOS; i++)
-  //   servos[i].detach();
+   for (int i = 0; i < NUM_SERVOS; i++)
+     servos[i].detach();
 }
 
 void activateServo(Servos s)
 {
-  // servos[s].attach(info[s][PIN]);
+   servos[s].attach(info[s][PIN]);
   servos[s].write(info[s][ACTIVATED]);
   delay(info[s][SLEW]);
   servos[s].write(info[s][NORMAL]);
   delay(info[s][SLEW]);
-  // servos[s].detach();
+   servos[s].detach();
 }
 
 void loop()
 {
-/*
   currentTime = millis();
+  if(currentTime - previousReset > servoResetInterval)
+  {
+    for (int i = 0; i < NUM_SERVOS; i++)
+    {
+      servos[i].attach(info[i][PIN]);
+      servos[i].write(info[i][NORMAL]);
+    }
+    delay(50);
+    for (int i = 0; i < NUM_SERVOS; i++)
+      servos[i].detach();
+    previousReset = currentTime;
+  }
+/*
   if(currentTime - previousTransmit > transmitInterval)
   {
     float supplyTemp = supplyDHT.readTemperature(true);
@@ -124,6 +137,6 @@ void loop()
       digitalWrite(HL, LOW);
     command = "";
   }
-
+  
   delay(1);
 }
