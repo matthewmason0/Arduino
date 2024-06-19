3bd944209a6223213e605554e700e08014ef05e9
Matt Mason <matthewmason0@gmail.com>
2024-06-18 23:18:17 -0500

    Add DISABLE_PRINT macro support, to allow optimizing out debug statements

--- libraries/custom_print/custom_print.h
+++ libraries/custom_print/custom_print.h
@@ -6,8 +6,10 @@ void print() {}
 template <typename T, typename... Types>
 void print(T arg, Types... others)
 {
+#ifndef DISABLE_PRINT
     Serial.print(arg);
     print(others...);
+#endif
 }
 
 void println() { Serial.println(); }
@@ -15,8 +17,10 @@ void println() { Serial.println(); }
 template <typename T, typename... Types>
 void println(T arg, Types... others)
 {
+#ifndef DISABLE_PRINT
     Serial.print(arg);
     println(others...);
+#endif
 }
 
 #endif // CUSTOM_PRINT_H
